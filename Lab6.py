82f7be0bfff43a4e486d6b181a3d2ef5a63500ce
BalajiDIyer <balaji.iyer@ufl.edu>
2023-03-22 19:35:15 -0400

    added decode fuction

--- Lab6.py
+++ Lab6.py
@@ -21,5 +21,5 @@ def decode(epw):
         elif epw[x] == "0":
             pw += "7"
         else:
-            pw += str(int(epw[x])-3) #subtracts
+            pw += str(int(epw[x])-3) #subtracts 3
     return pw
